1f4aeb5b389e8bc07a50bf61dc8d8a092b6217a6
Mohamed Atef <proofesser@gmail.com>
2020-01-22 04:41:36 -0800

    initialized dependency injection project using dagger library

---
+++ app/src/main/java/mo/atef/dagger/daggerapp/MainActivity.java
@@ -0,0 +1,14 @@
+package mo.atef.dagger.daggerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
